aa3c65e9a14cbc927186e53906f44369dea3c81b
shima-ui <79550616+shima-ui@users.noreply.github.com>
2021-02-25 12:51:51 +0900

    ver 1.0
    一旦完成したのでコミット

--- javascript.js
+++ javascript.js
@@ -1,19 +1,14 @@
-/*ツイートの長さ*/
-var tweet_length;
-
 /*作品名*/
 var work_name ="";
 
 /*ツイート内容*/
 var tweet_product;
 
-
 /*作品名取得＆表示*/
 function tag(tag_name){
     work_name = tag_name;
     document.getElementById("work_name_display").value = work_name;
 }
-
 
 /*ツイートコピー*/
 function copy(){
@@ -22,18 +17,22 @@ function copy(){
     document.execCommand("copy");
 }
 
-
 /*自動関数*/
 function realtime(){
+    // ツイート内容の生成
     tweet_product = `${work_name}　#wj${document.getElementById('no').value}\n${document.getElementById('tweetdraft').value}`;
 
-    tweet_length = 140 - tweet_product.length;
-    document.getElementById("tweet_length").value =  tweet_length;
+    // 文字数の計測と表示
+    document.getElementById("tweet_length").value =  140 - tweet_product.length;
 
+    // 文字数に応じたスタイル
+    var p = document.getElementById('tweet_length');
     if(tweet_length < 0){
-        document.getElementById("tweet_length").classList.add("tweet_length_over");
-        }else{
-            document.getElementById("tweet_length").classList.remove("tweet_length_over");
+        p.style.backgroundColor = "#E73A38";
+    }else if(tweet_length < 11){
+        p.style.backgroundColor = "#FFAD34";
+    }else{
+        p.style.backgroundColor = "#07A1EF";
     }
 }
 
@@ -42,4 +41,5 @@ function form_clear(){
     document.getElementById("tweetdraft").value = "";
 }
 
+// 自動関数の実行
 setInterval(realtime, 100);
